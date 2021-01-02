d5fbb6d1c9f596d6a45fddea989b4aa8664d6e3c
Stefan Evans <stefan.evans@consegna.cloud>
2021-01-03 08:45:08 +1300

    added props

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
       <div className="App">
         <h1>Hello World 2</h1>
         <p>WOrkign</p>
-        <Person />
+        <Person name="Anne" age="21"/>
+        <Person name="Tom" age="41">My Hobbies:Racing</Person>
+        <Person name="Jack" age="11"/>
       </div>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null , 'Some text'));
