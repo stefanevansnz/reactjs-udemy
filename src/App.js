297efaf01ea55c0d4036528db0f4844221de09a3
Stefan Evans <stefan.evans@consegna.cloud>
2021-01-03 08:33:09 +1300

    Added Person component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
@@ -7,6 +8,7 @@ class App extends Component {
       <div className="App">
         <h1>Hello World 2</h1>
         <p>WOrkign</p>
+        <Person />
       </div>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null , 'Some text'));
