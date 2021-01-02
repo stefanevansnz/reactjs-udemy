f005f1a1ce3c9938e5ce63011a64928fce3255f7
Stefan Evans <stefan.evans@consegna.cloud>
2021-01-03 08:22:14 +1300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World 2</h1>
+        <p>WOrkign</p>
+      </div>
+    );
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null , 'Some text'));
+  }
+}
+
+export default App;
