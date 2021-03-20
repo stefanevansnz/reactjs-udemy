6b61518343f536f5f9181108db8d256ebc6ed160
Stefan Evans <stefan.evans@consegna.cloud>
2021-03-20 16:48:08 +1300

    hook feature

--- src/App.js
+++ src/App.js
@@ -1,41 +1,43 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
-    persons: [ 
-      { name: 'Anne', age: 21},
-      { name: 'Tom', age: 53},
-      { name: 'Jack', age: 41}
-    ],
-    otherState: 'other value'
-  }
+const app = props => {
 
-  switchButtonHandler = () => {
-    //console.log('Was clicked');
-    this.setState( {
+    const [ personsState, setPersonsState ] = useState({
       persons: [ 
-        { name: 'Stef', age: 21},
+        { name: 'Anne', age: 21},
         { name: 'Tom', age: 53},
-        { name: 'Jack', age: 61}
-      ]
-    } );
-  }
+        { name: 'Jack', age: 41}
+      ]    
+    });
 
-  render() {
+    const [otherState, setOtherState] = useState('some other state');
+
+    console.log(personsState, otherState);
+
+    const switchButtonHandler = () => {
+      //console.log('Was clicked');
+      setPersonsState( {
+        persons: [ 
+          { name: 'Stef', age: 21},
+          { name: 'Tom', age: 53},
+          { name: 'Jack', age: 61}
+        ]
+      } );
+    }    
+
     return (
       <div className="App">
         <h1>Hello World</h1>
         <p>Work in progress</p>
-        <button onClick={this.switchButtonHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies:Racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button onClick={switchButtonHandler}>Switch Name</button>
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies:Racing</Person>
+        <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
       </div>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null , 'Some text'));
-  }
 }
 
-export default App;
+export default app;
